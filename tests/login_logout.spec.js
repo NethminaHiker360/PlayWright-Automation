6a3e28dc6830823f1195a1cfa466dbb796187990
NethminaHiker360 <you@example.com>
2025-12-01 16:59:29 +0530

    Add login/logout test for user authentication flow

--- tests/login_logout.spec.js
+++ tests/login_logout.spec.js
@@ -2,11 +2,24 @@ import { test, expect } from '@playwright/test';
 import { testURL } from '../common.js';
 
 test('login test', async ({ page }) => {
+  // Go to website
   await page.goto(testURL());
+
+  // Verify Sign In page is displayed
+  await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible();
+
+  // Perform login
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill('Administrator');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('123');
   await page.getByRole('button', { name: 'Sign In' }).click();
+
+  // Verify Time Tracker page is displayed
+  await expect(page.getByRole('heading', { name: 'Enter Employee Number' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Clock In/Out' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Logout' })).toBeEnabled();
+
+  // Perform logout
   await page.getByRole('button', { name: 'Logout' }).click();
 });
