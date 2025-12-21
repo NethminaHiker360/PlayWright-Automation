f642676b7083c54b423907d0082dc5f77b186217
NethminaHiker360 <you@example.com>
2025-12-21 19:42:06 +0530

    new window / new tab

---
+++ tests/logout.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+import { chromium } from "@playwright/test";
+import { timeTrackerURL } from "../common.js";
+
+test("Todo Test", async () => {
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  page.goto(timeTrackerURL());
+  await page.getByRole("textbox", { name: "Username" }).click();
+  await page.getByRole("textbox", { name: "Username" }).fill("Administrator");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill("123");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await page.getByRole("textbox", { name: "Enter ID" }).fill("12");
+
+
+  // Open a new tab in the same context --> same session new tab
+  const newPage01 = await context.newPage();
+  await newPage01.goto(timeTrackerURL());
+  await newPage01.pause();
+
+
+  // Open a new window with a new context --> incognito window
+  const newContext = await browser.newContext();
+  const newPage = await newContext.newPage();
+  await newPage.goto(timeTrackerURL());
+  await newPage.pause();
+
+});
